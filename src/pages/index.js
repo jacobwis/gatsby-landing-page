c966ce9089c326d063736a34e228d69d5ab6b733
Jacob Wisniewski <wisniewskij26@gmail.com>
2019-06-15 12:56:33 -0500

    finishing touches

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import React from "react"
+import Helmet from "react-helmet"
 import "../styles/index.scss"
 import Nav from "../components/Nav"
 import HeroSection from "../components/HeroSection"
 import BrandSection from "../components/BrandSection"
 import FeatureSection from "../components/FeatureSection"
+import favicon from "../images/favicon.png"
 
 const IndexPage = () => (
-  <div>
-    <Nav />
-    <HeroSection />
-    <BrandSection />
-    <FeatureSection />
-  </div>
+  <React.Fragment>
+    <Helmet
+      title="Braintree Landing Page Concept"
+      link={[{ rel: "shortcut icon", type: "image/png", href: `${favicon}` }]}
+    />
+    <div>
+      <Nav />
+      <HeroSection />
+      <BrandSection />
+      <FeatureSection />
+      <div className="Footer">
+        <div>
+          <p className="Footer__text">
+            Created by <a href="#">Jacob Wisniewski</a>
+          </p>
+        </div>
+        <div>
+          <a href="#">View Code</a>
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
 )
 
 export default IndexPage
